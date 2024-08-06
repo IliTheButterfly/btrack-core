82c71230dfa7d1fff9becffcf0ca5246ab60a0b6
civelier <civelier.algon@gmail.com>
2024-08-06 10:33:48 -0400

    Added boost

--- btrack-core/btrack-core/processing/wrapper.h
+++ btrack-core/btrack-core/processing/wrapper.h
@@ -7,11 +7,18 @@
 #include <opencv2/cudaimgproc.hpp>
 
 #define proc cv::cuda
+using procMat = cv::cuda::GpuMat;
+#define procDownload(img, procImg) procImg.download(img)
+#define procUpload(img, procImg) procImg.upload(img)
 
 #else
 
 #include <opencv2/imgproc.hpp>
 #define proc cv
+using procMat = cv::Mat;
+#define procDownload(img, procImg) img = procImg
+#define procUpload(img, procImg) procImg = img
+
 
 #endif // USE_CUDA
 
--- btrack-core/tests/wrapper_tests.cpp
+++ btrack-core/tests/wrapper_tests.cpp
@@ -1,5 +1,16 @@
 #include <gtest/gtest.h>
 #include <processing/wrapper.h>
 
+TEST(WrapperTests, CvtTest)
+{
+	std::cout << TEST_JPG_LOCATION << std::endl;
+	cv::Mat img = cv::imread(TEST_JPG_LOCATION);
+	procMat src, dst;
+	procUpload(img, src);
+	
+	proc::cvtColor(src, dst, cv::COLOR_BGR2RGB);
 
+	cv::Mat res;
+	procDownload(res, dst);
+}
 
